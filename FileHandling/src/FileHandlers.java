75287340eac5edb75585a437d1378e64af69da5a
krlwbstr <44670608+w8krl@users.noreply.github.com>
2022-01-09 22:35:34 +0000

    First commit

--- FileHandling/src/FileHandlers.java
+++ FileHandling/src/FileHandlers.java
@@ -11,6 +11,66 @@ public class FileHandlers {
 
         // Example read from file - use try catch to make handle missing file exception
         printFromFile("out.txt");
+
+        // Read from CSV
+        String[][] csv = readCsvFile("biostats.csv");
+
+        System.out.println("Finished!");
+
+    }
+
+    public static String[][] readCsvFile(String fileName){
+        String[][] res;
+
+        int colCount = 0;
+        int rowCount = 0;
+
+        //Set up placeholder for CSV values - get the col and row counts
+        try(BufferedReader fr = new BufferedReader(new FileReader(fileName))){
+
+            String line = fr.readLine();
+            String[] cols;
+
+            while(line != null){
+                ++rowCount;
+                cols = line.split(",");
+                colCount = cols.length;
+                line = fr.readLine();
+            }
+        } catch(Exception e){
+            System.out.println("Errror reading file");
+
+        }
+
+        // Set up 2d array
+        res = new String[rowCount][colCount];
+
+        try(BufferedReader fr = new BufferedReader(new FileReader(fileName))){
+
+            String line = fr.readLine();
+
+            int i = 0; int j;
+
+            // Use as main loop to read each line of file
+            while(line != null){
+                String[] data = line.split(",");
+
+                //Loop through current column
+                for(j=0; j<data.length; j++){
+                    res[i][j] = data[j];
+                }
+                // Read next line and increment row (i)
+                line = fr.readLine();
+                ++i;
+
+            }
+        } catch(Exception e){
+            System.out.println("Errror reading file");
+
+        }
+
+
+        return res;
     }
 
     public static void writeToFile(String file, String message){
@@ -44,6 +104,8 @@ public class FileHandlers {
         }
 
     }
+
+
 
 
 }
