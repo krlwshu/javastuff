7dba623ddfd227278699b43b77532ae4cc6bcfe3
krlwbstr <44670608+w8krl@users.noreply.github.com>
2022-01-09 15:32:54 +0000

    Adding complex data types

--- ComplexDataTypes/src/Vectors.java
+++ ComplexDataTypes/src/Vectors.java
@@ -1,5 +1,22 @@
+import java.util.Vector;
+
 public class Vectors {
 
+    public static void main(String[] args) {
 
+        Vector<String> Student = new Vector<String>();
+
+        Student.add("test");
+        Student.add("test");
+        Student.add(0,"JumpingToFirst!");
+        Student.add("JumpingToFirst!");
+        Student.addElement("cat");
+
+        int index = Student.indexOf("JumpingToFirst!");
+        System.out.println("the index of Jumpting to first! is: " +index);
+
+    }
 
 }
+
+
