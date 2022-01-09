3f0bdee3deefa09f98eaf9ebe843f95fc8b69cbd
krlwbstr <44670608+w8krl@users.noreply.github.com>
2022-01-09 16:54:29 +0000

    Adding complex data types

---
+++ Arrays/src/Array.java
@@ -0,0 +1,18 @@
+public class Array {
+
+    public static void main(String[] args) {
+
+        //String array
+        String[] stringArr = {"My","name","is","Elon"};
+        for(String word: stringArr){
+            System.out.println(word);
+        }
+
+        //Number array
+        int[] intArr = {1,2,3,4,5,7,6,5,4};
+        for(int number: intArr){
+            System.out.println(number);
+        }
+
+    }
+}
