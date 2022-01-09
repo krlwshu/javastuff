04336cca69fdabd3796cb2cd3ef5423084c147c4
krlwbstr <44670608+w8krl@users.noreply.github.com>
2022-01-09 15:42:32 +0000

    Adding complex data types

--- ComplexDataTypes/src/Vectors.java
+++ ComplexDataTypes/src/Vectors.java
@@ -4,16 +4,20 @@ public class Vectors {
 
     public static void main(String[] args) {
 
-        Vector<String> Student = new Vector<String>();
+        Vector<String> Student = new Vector<>();
 
-        Student.add("test");
-        Student.add("test");
-        Student.add(0,"JumpingToFirst!");
-        Student.add("JumpingToFirst!");
+        Student.add("Test");
+        Student.add("Test123");
+        Student.add(0,"FirstPlace!");
+        Student.add("FirstPlace!");
         Student.addElement("cat");
 
+        //FirstPlace! is in index positions 0 and 3 but the above indexOf seems to only find the first instance
         int index = Student.indexOf("JumpingToFirst!");
-        System.out.println("the index of Jumpting to first! is: " +index);
+        System.out.println("the index of FirstPlace! is: " +index);
+
+        // The vector keeps tack of the position of the last element
+        System.out.println(Student.lastElement());
 
     }
 
